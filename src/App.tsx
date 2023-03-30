dc556d85191bbcfe5de010cbbc02865029f64a51
Alexander Ershov <player3755@mail.ru>
2023-03-31 00:08:55 +0300

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import "./App.css";
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
 
+const App = () => {
+    const [board, setBoard] = useState(new Board());
+
+    useEffect( () => {
+        
+    })
+
+    return (
+        <div className='app'>
+            <BoardComponent/>
+        </div>
+    )
+}
 
 export default App;
-//опроп
---
+++ src/models/Cell.ts
@@ -0,0 +1,26 @@
+import { NumberLiteralType } from "typescript";
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Cell {
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean; // aviable to move
+    id: Number; // for react keys
+
+
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random()
+    }
+
+}
